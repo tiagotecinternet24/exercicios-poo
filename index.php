fb22541dd9208377e0103cfcffb7821545d07c18
bsantostiago <tiago.tecinternet2024@gmail.com>
2025-04-22 08:08:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Exercício 01</title>
+    <title>Exercício 02</title>
 </head>
 <body>
-    <h1>Exercício 01</h1>
+    <h1>Exercício 02</h1>
     <hr>
     <p><i>Faça as chamadas e testes nesta página</i></p>
 
